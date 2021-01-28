378c636937d560b165275a523f3ddaf407987233
Pacs <frjamezc@ucsc.edu>
2021-01-27 18:35:26 -0800

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,27 @@
+/*
+ * Author: Francisco Amezcua
+ * Created: 27 January
+ * Public Domain
+ */
+
+// Declare variables
+var make = "Nissan";
+var model = "Sentra";
+var color = "Silver";
+var year = 2016;
+var ownIt = true;
+
+console.log(make);
+console.log(model);
+console.log(color);
+console.log(year)
+console.log(ownIt);
+
+// Calculate
+var age = 2021 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: "  + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
---
+++ section/js/section.js
@@ -0,0 +1,4 @@
+var textAnalysis = "I'm a real boy";
+console.log(textAnalysis);
+var += ", aren't I?";
+console.log(textAnalysis);
