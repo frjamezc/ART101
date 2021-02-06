ac8910da30121dbd92cd71ea92bf63aa12baa7e5
Pacs <frjamezc@ucsc.edu>
2021-02-06 09:18:40 -0800

    Lab 8

--- sectionD/js/section.js
+++ sectionD/js/section.js
@@ -1,1 +1,8 @@
-console.log("JavaScript loaded");
+function userInfo(first, last) {
+  first = first[0].toUpperCase() + first.substring(1).toLowerCase();
+  last = last[0].toUpperCase() + last.substring(1).toLowerCase();
+  return "My first name is " + first +
+          " and my last name is " + last;
+}
+
+document.writeln(userInfo("FrAnCiScO", "AmEzCuA"));
