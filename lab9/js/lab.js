16481cc49e2eac4dcc2cf80c60cda966ead2afef
Pacs <frjamezc@ucsc.edu>
2021-02-10 22:45:21 -0800

    typo

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -9,7 +9,7 @@ var new1El = document.createElement("p"); // Creates <p> element
 new1El.innerHTML = "Poggers!"; // Sets the P tag's text
 new1El.setAttribute("style","color: red"); // makes the p red
 var new2El = document.createElement("h4"); // creates <h4> element
-new2El.innerHTML ="This is text is in an h4 tag!"; // set h4 text
+new2El.innerHTML ="This text is in an h4 tag!"; // set h4 text
 new2El.setAttribute("style","color: red"); // make h4 red
 outputEl.appendChild(new1El);
 outputEl.appendChild(new2El);
