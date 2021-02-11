d4ef850e63fdcbcf90860cafe53773fbfa6aa347
Pacs <frjamezc@ucsc.edu>
2021-02-10 22:43:49 -0800

    Lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,17 @@
+/*
+Author: Francisco Amezcua
+Date: 02/10/2021
+Public License
+*/
+
+var outputEl = document.getElementById("output"); // Get element
+var new1El = document.createElement("p"); // Creates <p> element
+new1El.innerHTML = "Poggers!"; // Sets the P tag's text
+new1El.setAttribute("style","color: red"); // makes the p red
+var new2El = document.createElement("h4"); // creates <h4> element
+new2El.innerHTML ="This is text is in an h4 tag!"; // set h4 text
+new2El.setAttribute("style","color: red"); // make h4 red
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
+
+document.writeln(outputEl);
