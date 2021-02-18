2572221769f673be098ba0b0046fbfc20ecc2a1a
Pacs <frjamezc@ucsc.edu>
2021-02-17 19:54:54 -0800

    Lab11

---
+++ lab11/js/lab.js
@@ -0,0 +1,23 @@
+/*
+  author: Francisco Amezcua
+  date: 17 February 2021
+  license: MIT
+*/
+
+$(document).ready(function() { // Alter document
+  $('#challenge, #problems, #results').append(
+    $(document.createElement('button')).prop({
+      type: 'button',
+      innerHTML: 'Press me'
+    })
+  );
+  $('#challenge button').click(function(){
+    $('#challenge .special').toggle();
+  })
+  $('#problems button').click(function(){
+    $('#problems .special').toggle();
+  })
+  $('#results button').click(function(){
+    $('#results .special').toggle();
+  })
+});
