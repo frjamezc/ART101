fb96f6050211a49bedeb743ae311fa6dcdf4c529
Pacs <frjamezc@ucsc.edu>
2021-02-17 21:22:04 -0800

    Comments for lab js code

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -4,22 +4,24 @@
   license: MIT
 */
 
-$(document).ready(function() { // Alter document
-  $('#challenge, #problems, #results').append(
+$(document).ready(function() { // Choose document
+  $('#challenge, #problems, #results').append( // Add buttons to each div w/ ID
+    // Button creation
     $(document.createElement('button')).prop({
       type: 'button',
       innerHTML: 'Press me'
     })
   );
+  // Toggle hide() show() div of class special
   $('#challenge button').click(function(){
     $('#challenge .special').toggle();
-    $('#challenge button').css('background-color', 'white');
-    $('#problems button').css('background-color','blue');
+    $('#challenge button').css('background-color', 'white'); // BONUS
+    $('#problems button').css('background-color','blue'); // BONUS
   })
   $('#problems button').click(function(){
     $('#problems .special').toggle();
-    $('#problems button').css('background-color', 'white');
-    $('#challenge button').css('background-color','red');
+    $('#problems button').css('background-color', 'white'); // BONUS
+    $('#challenge button').css('background-color','red'); // BONUS
   })
   $('#results button').click(function(){
     $('#results .special').toggle();
