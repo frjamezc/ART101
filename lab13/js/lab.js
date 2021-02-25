324eb44ec5a34b58fc2cdda32dc465c30f6d40d8
Pacs <frjamezc@ucsc.edu>
2021-02-24 20:14:16 -0800

    Lab13

---
+++ lab13/js/lab.js
@@ -0,0 +1,41 @@
+/*
+Author: Francisco Amezcua
+Date: 02/24/21
+Public License
+*/
+
+function fizzBuzz() {
+  var output = "";
+  for(i = 0; i <= 200; i++)
+  {
+    if(i%3 == 0 && i%5 == 0 && i%7 == 0)
+    {
+      output += i + " - FizzBuzzBoom!<br>";
+    }
+    else if(i%3 == 0 && i%5 == 0) {
+      output += i + " - FizzBuzz!<br>";
+    }
+    else if (i%3 == 0 && i%7 == 0) {
+      output += i + " - FizzBoom!<br>";
+    }
+    else if (i%5 == 0 && i%7 == 0) {
+      output += i + " - BuzzBoom!<br>";
+    }
+    else if (i%3 == 0) {
+      output += i + " - Fizz!<br>";
+    }
+    else if (i%5 == 0) {
+      output += i + " - Buzz!<br>";
+    }
+    else if (i%7 == 0) {
+      output += i + " - Boom!<br>";
+    }
+    else
+    {
+      output += i + " -<br>"
+    }
+  }
+  $("#output").html(output);
+}
+
+fizzBuzz();
